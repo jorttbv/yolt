49d92132e92beca52d9e07699164267810fdbab7
Bob Forma <bobforma@gmail.com>
2020-03-11 13:42:46 +0100

    Support all client/server errors

--- lib/yolt/error.rb
+++ lib/yolt/error.rb
@@ -4,13 +4,36 @@ module Yolt
   class Error < StandardError
     attr_reader :status_code, :code
 
+    # client errors
     ClientError = Class.new(self)
+    BadRequest = Class.new(ClientError)
+    Unauthorized = Class.new(ClientError)
+    Forbidden = Class.new(ClientError)
     NotFound = Class.new(ClientError)
+    MethodNotAllowed = Class.new(ClientError)
+    NotAcceptable = Class.new(ClientError)
+    Conflict = Class.new(ClientError)
     Gone = Class.new(ClientError)
 
+    # server errors
+    ServerError = Class.new(self)
+    InternalServerError = Class.new(ServerError)
+    ServiceUnavailable = Class.new(ServerError)
+
     STATUS_CODE_TO_ERROR_CLASS = {
-      404 => NotFound,
-      410 => Gone,
+      # client errors
+      StatusCode::BAD_REQUEST => BadRequest,
+      StatusCode::UNAUTHORIZED => Unauthorized,
+      StatusCode::FORBIDDEN => Forbidden,
+      StatusCode::NOT_FOUND => NotFound,
+      StatusCode::METHOD_NOT_ALLOWED => MethodNotAllowed,
+      StatusCode::NOT_ACCEPTABLE => NotAcceptable,
+      StatusCode::CONFLICT => Conflict,
+      StatusCode::GONE => Gone,
+
+      # server errors
+      StatusCode::INTERNAL_SERVER_ERROR => InternalServerError,
+      StatusCode::SERVICE_UNAVAILABLE => ServiceUnavailable,
     }.freeze
 
     class << self
